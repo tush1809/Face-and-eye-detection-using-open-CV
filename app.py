15b2a18e2093379bfd14147b29efdee673beaca1
Tushar <96531426+tush1809@users.noreply.github.com>
2025-09-09 11:27:19 +0530

    added vc

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, Response
 import cv2
 app=Flask(__name__)
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(static/sample.mp4)
 
 def gen_frames():
     while True:
